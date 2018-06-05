858668b20dfc70e1aa998484640f461fec7ca441
Corey H <c.l.halicke@gmail.com>
2018-06-05 15:43:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function johnLennonFacts(facts) {
 
 //DONE The condition of the loop should check to see that the parameter number is less than 15. 
 
-//The function should return the array with the strings "I love the Beatles!".
+//DONE The function should return the array with the strings "I love the Beatles!".
 
 
 
